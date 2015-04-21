6c0f512cfcb85d18727de1b017e65473cd0d5225
shasto4ka <shasto4ka@mail.ru>
2015-04-21 21:06:38 +0300

    3_04_metric_settings_add

--- app/src/main/java/com/example/shasta/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/shasta/sunshine/DetailActivity.java
@@ -42,7 +42,7 @@ public class DetailActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            startActivity(new Intent(this , SettingsActivity.class ) );
+            startActivity(new Intent(this ,SettingsActivity.class ) );
             return true;
         }
 
